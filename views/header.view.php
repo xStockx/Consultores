cfa254813058eace584086c2816d79203876ffad
xStockx <javier@rains.cl>
2014-12-22 18:37:04 -0300

    1

--- views/header.view.php
+++ views/header.view.php
@@ -9,8 +9,8 @@
 				<li><a href="index.php">Inicio</a></li>
 				<li><a href="sobreNosotros.php">Sobre Nosotros</a></li>
 				<li><a href="">Nuestro Equipo</a></li>
-				<li><a href="">Servicios</a></li>
-				<li><a href="">Contacto</a></li>
+				<li><a href="servicios.php">Servicios</a></a></li>
+				<li><a href="contacto.php">Contacto</a></li>
 				<!-- <li></li>
 				<li></li> -->
 			</ul>
