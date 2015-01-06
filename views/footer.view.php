a0fc6b984069fc1fba34650a1a2ee21308a50757
xStockx <javier@rains.cl>
2015-01-06 09:39:46 -0300

    a

--- views/footer.view.php
+++ views/footer.view.php
@@ -2,7 +2,7 @@
 		<p>ADLH Consultores - 
 		Agustinas 715 oficina 604, Santiago Centro -
 		Teléfono : 2632 6009</p>
-		<p id="derechos">Desarrollado por rains.cl</p>
+		<p id="derechos"><span class="icon-IE"> </span><span class="icon-chrome"> </span><span class="icon-firefox"> </span><span class="icon-opera"> </span><span class="icon-safari"> </span>Desarrollado por <a href="#">rains.cl </a></p>
 	</footer>
 </body>
 </html>
